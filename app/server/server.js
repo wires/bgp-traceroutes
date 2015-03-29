aa88f972461465c6cdeb76f54eca7ef7133a5f02
wires <jelle@defekt.nl>
2015-03-29 15:09:23 +0200

    D3 + geojson

--- app/server/server.js
+++ app/server/server.js
@@ -1,6 +1,9 @@
 var mach = require('mach');
 var app = mach.stack();
 var c = require("chalk");
+var _ = require("lodash");
+
+var GeoJSON = require("geojson");
 
 var atlas = require("./atlas");
 
@@ -32,6 +35,15 @@ app.get('/anchors', function(conn){
 		.then(OK(conn));
 });
 
+app.get('/anchors.geojson', function(conn){
+	return atlas
+		.anchors()
+		.then(function(anchors){
+			return GeoJSON.parse(anchors, {Point: ['latitude', 'longitude']});
+		})
+		.then(OK(conn));
+});
+
 app.get('/reach', function(conn){
 	var q = conn.params.q;
 	if(!q)
@@ -50,6 +62,50 @@ app.get('/reach', function(conn){
 
 	return 400;
 });
+
+
+app.get('/reach.geojson', function(conn){
+	var q = conn.params.q;
+	var lat = parseFloat(conn.params.lat);
+	var lng = parseFloat(conn.params.lng);
+	
+	if(!q)
+		return 400;
+
+	// ip/prefix ~ v4/v6
+	if(q.match(/[0-9\.\:]+\/\d+/)){
+		var ip = q.split('/')[0]
+		var prefix = q.split('/')[1]
+		console.log("querying prefix " + c.cyan(ip) +
+								"/" + c.red(prefix));
+		return atlas
+			.bgp_endpoints(ip + '/' + prefix)
+			.then(function(as){
+				var probes = _(as)
+					.map(function(n){
+						return n.probes;
+					})
+					.flatten()
+					.value();
+				
+				// add lines to the origin	
+				if(lat && lng)
+				{
+					var lines = probes.map(function(p){
+						return {line: [[lat, lng], [p.latitude, p.longitude]]}
+					});
+					
+					probes = probes.concat(lines);
+				}
+					
+				return GeoJSON.parse(probes, {Point: ['latitude', 'longitude'], 'LineString': 'line'})
+			})
+			.then(OK(conn));
+	};
+
+	return 400;
+});
+
 
 app.use(mach.file, {
 	root: process.cwd() + '/../dist/',
--- app/src/js/testme.js
+++ app/src/js/testme.js
@@ -38,6 +38,7 @@ L.mapbox.accessToken = 'pk.eyJ1Ijoid2lyZXMiLCJhIjoiUk54Vlh5RSJ9.3BVTFfeHzPRbgBye
     
     // var map = L.mapbox.map('map', 'examples.map-zr0njcqy');
 var map = L.mapbox.map('map', 'wires.lj6fgb39')
+    .setView([42, 22], 2)
 
 map.featureLayer.on('click', function(e) {
     map.panTo(e.layer.getLatLng());
@@ -91,7 +92,9 @@ api('/anchors', {}, function(anchors){
         m.on('click', function(e){
             console.log("clicked", a);
             
-            api('/reach', {q: a.prefix_v4}, function(reach){
+            doReach(a.prefix_v4, a.latitude, a.longitude);
+            
+            if(true) api('/reach', {q: a.prefix_v4}, function(reach){
                 console.log("DONE", reach);
                 
                 reach.forEach(function(as){
@@ -117,6 +120,91 @@ api('/anchors', {}, function(anchors){
 });
 
 
+
+// var map = new L.Map("map", {center: [37.8, -96.9], zoom: 4})
+//     .addLayer(new L.TileLayer("http://{s}.tiles.mapbox.com/v3/examples.map-vyofok3q/{z}/{x}/{y}.png"));
+// 
+
+function doReach(prefix, lat, lng){
+    
+    console.log("REACH");
+    
+    var svg = d3.select(map.getPanes().overlayPane).append("svg");
+    var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+    api("/reach.geojson", {q: prefix, lat: lat, lng: lng}, function(collection) {
+        
+      var transform = d3.geo.transform({point: projectPoint}),
+          path = d3.geo.path().projection(transform);
+
+      var feature = g.selectAll("path")
+          .data(collection.features)
+        .enter().append("path");
+
+        feature.on('click', function(d) {
+            console.log("CLICKED", d);
+        });
+
+      map.on("viewreset", reset);
+      reset();
+
+      // Reposition the SVG to cover the features.
+      function reset() {
+        var bounds = path.bounds(collection),
+            topLeft = bounds[0],
+            bottomRight = bounds[1];
+
+        svg .attr("width", bottomRight[0] - topLeft[0])
+            .attr("height", bottomRight[1] - topLeft[1])
+            .style("left", topLeft[0] + "px")
+            .style("top", topLeft[1] + "px");
+
+        g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+        feature.attr("d", path);
+      }
+
+      // Use Leaflet to implement a D3 geometric transformation.
+      function projectPoint(x, y) {
+        var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+        this.stream.point(point.x, point.y);
+      }
+      
+    });
+}
+
+// console.log("HEERE")
+// d3.json("/anchors", function(collection) {
+//     console.log("HAHA", collection);
+
+  // var transform = d3.geo.transform({point: projectPoint}),
+  //     path = d3.geo.path().projection(transform);
+  // 
+  // var feature = g.selectAll("path")
+  //     .data(collection.features)
+  //   .enter().append("path");
+  // 
+  // map.on("viewreset", reset);
+  // reset();
+  // 
+  // // Reposition the SVG to cover the features.
+  // function reset() {
+  //   var bounds = path.bounds(collection),
+  //       topLeft = bounds[0],
+  //       bottomRight = bounds[1];
+  // 
+  //   svg .attr("width", bottomRight[0] - topLeft[0])
+  //       .attr("height", bottomRight[1] - topLeft[1])
+  //       .style("left", topLeft[0] + "px")
+  //       .style("top", topLeft[1] + "px");
+  // 
+  //   g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+  // 
+  //   feature.attr("d", path);
+  // }
+  // 
+// 
+// });
 
 
 
